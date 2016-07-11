5d204dbd6377a2828abd791c3741acfb5b0185b9
fangxiaoyu <505558491@qq.com>
2016-07-11 19:20:34 +0800

    branch 3e

--- hello2a.py
+++ hello2a.py
@@ -1,9 +1,22 @@
 from flask import Flask,request,make_response,redirect,abort,render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'),500
 
 @app.route('/agent')
 def agent():
@@ -14,7 +27,7 @@ def agent():
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html',current_time = datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
